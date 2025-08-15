52338de0ddf79a9bb64aae776e29b719b7394a27
palak chimnani <pchimnani81@gmail.com>
2025-08-15 17:14:11 +0530

    all activities

--- app/src/main/java/com/example/canvascity/Activity/WalletActivity.java
+++ app/src/main/java/com/example/canvascity/Activity/WalletActivity.java
@@ -1,4 +1,4 @@
-package com.example.canvascity;
+package com.example.canvascity.Activity;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.canvascity.R;
+
+public class WalletActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_wallet);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
